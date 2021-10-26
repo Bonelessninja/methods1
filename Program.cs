203efd9124ee9b2859f979e2e0542a732c12da81
Alex Fenton <fentona390@gmail.com>
2021-10-26 11:30:37 -0300

    update

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace methods
+{
+    class Program
+    {
+        static int score;
+        static string name;
+        static int gold;
+        static int level;
+        static void Main(string[] args)
+        {
+            Console.WriteLine("====================");
+            Console.WriteLine("score = " + score);
+            Console.WriteLine("name = " + name);
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("methods:");
+            Console.WriteLine();
+
+            //initialize
+            score = 0;
+            name = "boneless";
+            xp
+            
+        }
+    }
+}
